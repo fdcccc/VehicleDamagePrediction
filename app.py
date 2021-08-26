d6c3c37f830003d7e16bf93090b7715a45415787
fdcccc <82291204+fdcccc@users.noreply.github.com>
2021-08-25 20:41:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
 import streamlit as st
-import numpy as np
+
 import pandas as pd
-import yfinance as yf
-import plotly.express as px
+import numpy as np
+import dill
+import geopandas as gpd
+
+import json
+import bokeh.io
+
+
+from bokeh.io import show
+from bokeh.models import GeoJSONDataSource,ColumnDataSource,LogColorMapper,ColorBar,LinearColorMapper
+from bokeh.plotting import figure
+from bokeh.sampledata.sample_geojson import geojson
+from bokeh.palettes import Blues8  as palette
+from bokeh.models import HoverTool
 
 
 
-# In[ ]:streamlit run 12.py
-
-st.title('Price Chart')
-id = st.sidebar.text_input("Enter Stock Ticke (example: AAPL)")
-stock = yf.Ticker(id)
-df=stock.history(period='max')['Close']
-if len(df)==0 and len(id)>0:
-    st.write('Invalid Stock Ticker')
-else:
-    df.index=pd.DatetimeIndex(df.index)
-    year= st.sidebar.selectbox('Year', tuple(sorted(set(df.index.year))) )
-    
-    month= st.sidebar.selectbox("Month", tuple(sorted(set(df[df.index.year==year].index.month))) )
+# In[8]:
 
 
+model_crime=dill.load(open('model_crime.pkd','rb'))
+model_crash=dill.load(open('model_crash.pkd','rb'))
+current=dill.load(open('current.pkd', 'rb'))
+model_all_crime=dill.load(open('model_all_crime.pkd','rb'))
+model_all_crash=dill.load(open('model_all_crash.pkd','rb'))
+current_all=dill.load(open('current_all.pkd', 'rb'))
+his=dill.load(open('history.pkd','rb'))
 
-    try:
-        year=int(year)
-        month=int(month)
-        df2=df[(pd.DatetimeIndex(df.index).year==year) & (pd.DatetimeIndex(df.index).month==month) ].reset_index()
-        df2.columns=['Date','Price']
+with st.sidebar:  
+    st.write("## Select month and days of the week to see the prediciton in the next 6 months.")
+    Month_sel=st.selectbox("Month", ('2021-08','2021-09','2021-10','2021-11','2021-12','2022-01') )
+    dw=st.selectbox("Days of the Week", ('Weekdays','Weekends','Everyday')) 
+    st.text("")
+    st.text("")
 
-        st.write(px.line(df2, x='Date', y='Price', title=str(id)+" "+ str(year) +"-"+str(month)))
-    except:
-        pass
+    st.info("The prediction can be shown by incident numbers or incident rates (per 10,000 population)")
+    st.write("## Select prediction type")
+    im=st.radio('',['Numbers' , 'Rates'])
     
